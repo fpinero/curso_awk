f71badcaf08a26b15276422970e313a6dad19b4d
Fernando Piñero Estrada <git>
2023-02-02 14:39:22 +0100

    añadidos aún más recursos

--- myIP.sh
+++ myIP.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
 
-ifconfig | awk 'BEGIN{FS=" "; RS="\n"} { if ($1 == "inet" && $2 != "127.0.0.1" ) {print "la IP del equipo es: " $2}}
+ifconfig | awk 'BEGIN{FS=" "; RS="\n"} { if ($1 == "inet" && $2 != "127.0.0.1" ) {print "la IP del equipo es: " $2}}'
 
-'
